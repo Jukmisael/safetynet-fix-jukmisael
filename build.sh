f52df39b21b2ae28bcc633b34ba8dd683aa1d189
jukmisael@gmail.com <jukmisael@gmail.com>
2023-01-11 18:07:03 -0300

    test action

--- build.sh
+++ build.sh
@@ -38,23 +38,34 @@ sha256sum classes.dex | cut -d' ' -f1 | tr -d '\n' > classes.dex.sha256sum
 rm -f "$src_dir/safetynet-fix-riru.zip"
 
 zip -r9 "$src_dir/safetynet-fix-riru.zip" .
-    
 
-pushd "$src_dir/zygisk"
+set -euo pipefail
 
-rm -fr out
-chmod +x ./gradlew
+build_mode="${1:-release}"
 
-./gradlew "assemble$build_mode" -scan
-
+pushd zygisk/module
+rm -fr libs
+debug_mode=1
+if [[ "$build_mode" == "release" ]]; then
+    debug_mode=0
+fi
+/usr/local/lib/android/sdk/ndk/25.1.8937393/ndk-build -j48 NDK_DEBUG=$debug_mode
 popd
 
-unzip "$src_dir/zygisk/out/safetynet-fix-"*.zip
+pushd java
+# Must always be release due to R8 requirement
+./gradlew assembleRelease
+popd
 
-unzip "$src_dir/java_module/app/build/outputs/apk/release/app-release.apk" classes.dex
+mkdir -p magisk/zygisk
+for arch in arm64-v8a armeabi-v7a x86 x86_64
+do
+    cp "zygisk/module/libs/$arch/libsafetynetfix.so" "magisk/zygisk/$arch.so"
+done
 
-sha256sum classes.dex | cut -d' ' -f1 | tr -d '\n' > classes.dex.sha256sum
-
-rm -f "$src_dir/safetynet-fix-zygisk.zip"
-
-zip -r9 "$src_dir/safetynet-fix-zygisk.zip" .
+pushd magisk
+version="$(grep '^version=' module.prop  | cut -d= -f2)"
+rm -f "../safetynet-fix-$version.zip" classes.dex
+unzip "../java/app/build/outputs/apk/release/app-release.apk" "classes.dex"
+zip -r9 "../safetynet-fix-zygisk.zip" .
+popd
