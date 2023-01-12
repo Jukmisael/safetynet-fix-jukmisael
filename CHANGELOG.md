d5a0778beb878f048a6cef516e3a88de6a9ade8d
jukmisael@gmail.com <jukmisael@gmail.com>
2023-01-12 18:34:32 -0300

    revert on revert

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# v2.3.1.05 modo by Jukmisael
+## Highlights
+
+- Add Sepolicy rule
+
+# v2.3.1.04 mod by Jukmisael
+## Highlights
+
+- Organization of the services.sh
+- Added Hybrid support for Riru and Zygisk
+- Added support for hiding modifications in Init.rc and USB Debugging
+- Trying to hide addon.d not to show "Custom Rom"
+- Mount filesystem as RW to rename addon.dde improvements
+- Fix JsonUpdate Module files
+
+- Some modifications in Init.rc can not be hidden, this is a problem with the ROM
+
+---
+
+# Donate
+
+**If you found this module helpful, please consider supporting development with a [recurring donation](https://patreon.com/kdrag0n)** for rewards such as early access to updates, exclusive behind-the-scenes development news, and priority support. Alternatively, [you can also **buy me a coffee**](https://paypal.me/kdrag0ndonate). All support is appreciated ❤️
--- build.sh
+++ build.sh
@@ -1,34 +1,59 @@
-#!/usr/bin/env bash
-
 tmp_dir="$(mktemp --tmpdir -d modulebuild.XXXXXXXXXX)"
 cleanup() {
+
     rm -fr "$tmp_dir"
+
 }
+
 trap cleanup EXIT
 
 build_mode="${1:-Release}"
 
 pushd "$(dirname "$0")"
+
 src_dir="$(pwd)"
+
 popd
 
 cd "$tmp_dir"
 
 pushd "$src_dir/riru"
+
 rm -fr out
-chmod 777 *
+chmod +x ./gradlew
+
 ./gradlew "assemble$build_mode"
+
 popd
 
 pushd "$src_dir/java_module"
-# Must always be release due to R8 requirement
-chmod 777 *
-./gradlew assembleRelease
+
+rm -fr out
+chmod +x ./gradlew
+
+./gradlew "assemble$build_mode"
+
 popd
 
+ls
+
 unzip "$src_dir/riru/out/safetynet-fix-"*.zip
-unzip "$src_dir/java_module/app/build/outputs/apk/release/app-release.apk" classes.dex
+
+#unzip "$src_dir/java_module/app/build/outputs/apk/release/app-release.apk" classes.dex
+
 sha256sum classes.dex | cut -d' ' -f1 | tr -d '\n' > classes.dex.sha256sum
 
-rm -f "$src_dir/safetynet-fix.zip"
-zip -r9 "$src_dir/safetynet-fix.zip" .
+#rm -f "$src_dir/safetynet-fix.zip"
+
+#zip -r9 "$src_dir/safetynet-fix.zip" .
+ls
+
+version="$(grep '^version=' module.prop  | cut -d= -f2)"
+
+rm -f "$src_dir/safetynet-fix-$version.zip"
+
+unzip "$src_dir/java_module/app/build/outputs/apk/release/app-release.apk" classes.dex
+
+zip -r9 "$src_dir/safetynet-fix-$version.zip" .
+
+ls
