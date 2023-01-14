0b05933fe4c216d1a1ac35fae4560c13e8c3e855
Jukmisael <jukmisael@gmail.com>
2023-01-14 20:12:14 -0300

    fix name

--- build.sh
+++ build.sh
@@ -10,23 +10,18 @@ trap cleanup EXIT
 build_mode="${1:-Release}"
 
 pushd "$(dirname "$0")"
-
 src_dir="$(pwd)"
-
 popd
 
 cd "$tmp_dir"
 
 pushd "$src_dir/riru"
-
 rm -fr out
 chmod +x ./gradlew
 ./gradlew "assemble$build_mode"
-
 popd
 
 pushd "$src_dir/java_riru"
-
 rm -fr out
 chmod +x ./gradlew
 ./gradlew "assemble$build_mode"
@@ -34,24 +29,18 @@ ls
 popd
 
 pushd "$src_dir/zygisk"
-
 rm -fr out
 chmod +x ./gradlew
 ./gradlew "assemble$build_mode"
-ls
-
 popd
 
 pushd "$src_dir/java_zygisk"
-
 rm -fr out
 chmod +x ./gradlew
 ./gradlew "assemble$build_mode"
-ls
 popd
 
 unzip "$src_dir/riru/out/safetynet-fix-"*.zip
-ls
 
 #unzip "$src_dir/java_module/app/build/outputs/apk/release/app-release.apk" classes.dex
 
@@ -60,8 +49,14 @@ version="$(grep '^version=' module.prop  | cut -d= -f2)"
 rm -f "$src_dir/safetynet-fix-v"*.zip
 
 unzip "$src_dir/java_riru/app/build/outputs/apk/release/app-release.apk" classes.dex
-unzip "$src_dir/java_zygisk/app/build/outputs/apk/release/app-release.apk" zygisk_classes.dex
+
+pushd "$tmp_dir"
+unzip "$src_dir/java_zygisk/app/build/outputs/apk/release/app-release.apk" classes.dex
+popd
+
+mv "$tmp_dir/classex.dex" zygisk_classes.dex
 
 sha256sum classes.dex | cut -d' ' -f1 | tr -d '\n' > classes.dex.sha256sum
+sha256sum zygisk_classes.dex | cut -d' ' -f1 | tr -d '\n' > zygisk_classes.dex.sha256sum
 
 zip -r9 "$src_dir/safetynet-fix-$version.zip" .
