8fabcb05663a95fc43be4e886ca702c4764d57f3
Misael Filho <jukmisael@gmail.com>
2023-01-17 23:01:46 -0300

    Update SecurityBridge.kt

--- java_riru/app/src/main/java/dev/kdrag0n/safetynetriru/SecurityBridge.kt
+++ java_riru/app/src/main/java/dev/kdrag0n/safetynetriru/SecurityBridge.kt
@@ -1,7 +1,7 @@
-package dev.kdrag0n.safetynetrirujukmisael
+package dev.kdrag0n.safetynetriru
 
-import dev.kdrag0n.safetynetrirujukmisael.proxy.ProxyKeyStoreSpi
-import dev.kdrag0n.safetynetrirujukmisael.proxy.ProxyProvider
+import dev.kdrag0n.safetynetriru.proxy.ProxyKeyStoreSpi
+import dev.kdrag0n.safetynetriru.proxy.ProxyProvider
 import java.security.KeyStore
 import java.security.KeyStoreSpi
 import java.security.Security
