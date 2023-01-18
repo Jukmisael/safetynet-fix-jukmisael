68e22fa04403cfc234bf2015b254a8738cf94225
Misael Filho <jukmisael@gmail.com>
2023-01-18 02:21:56 -0300

    Update build.sh
    Fix version

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ build_module java_zygisk "$src_dir/java_zygisk" "$tmp_dir" "$1"
 wget -P "$tmp_dir" https://github.com/kdrag0n/safetynet-fix/releases/download/v2.4.0/safetynet-fix-v2.4.0.zip
 unzip "$tmp_dir/safetynet-fix-v2.4.0.zip" -d "$tmp_dir"
 
-version="$(grep '^version=' "$src_dir/module.prop"  | cut -d= -f2)"
+version="$(grep '^version=' "$tmp_dir/module.prop"  | cut -d= -f2)"
 sha256sum "$tmp_dir/classes.dex" | cut -d' ' -f1 | tr -d '\n' > "$tmp_dir/classes.dex.sha256sum"
 sha256sum "$tmp_dir/zygisk_classes.dex" | cut -d' ' -f1 | tr -d '\n' > "$tmp_dir/zygisk_classes.dex.sha256sum"
 
