5dd8e7e6fe12aaf9ea162030213df400d4fbf101
Misael Filho <jukmisael@gmail.com>
2023-01-17 23:24:03 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ trap cleanup EXIT
 
 build_mode="${1:-Release}"
 
-pushd "$(dirname "$0")" || exit
+pushd "$(dirname "$0")"
 src_dir="$(pwd)"
-popd || exit
+popd
 
 pushd "$src_dir/riru"
 rm -fr out
@@ -23,28 +23,32 @@ pushd "$src_dir/java_riru"
 rm -fr out
 chmod +x ./gradlew
 ./gradlew "assemble$build_mode"
+unzip "./app/build/outputs/apk/release/app-release.apk" classes.dex
+mv classes.dex "$src_dir/classes.dex"
 popd
 
-pushd "$src_dir/java_zygisk" || exit
+pushd "$src_dir/java_zygisk"
 rm -fr out
 chmod +x ./gradlew
 ./gradlew "assemble$build_mode"
-popd || exit
+unzip "$src_dir/java_zygisk/app/build/outputs/apk/release/app-release.apk" classes.dex
+mv classes.dex "$src_dir/zygisk_classes.dex
+popd
+
+pushd "$tmp_dir/"
+mkdir wsfn
+mkdir sfn
+wget -P "./wsfn" https://github.com/kdrag0n/safetynet-fix/releases/download/v2.4.0/safetynet-fix-v2.4.0.zip
+unzip "./wsfn/safetynet-fix-v2.4.0.zip" -d "./sfn"
+mv "./sfn/zygisk" "$src_dir/zygisk/"
+popd
+
 
 unzip "$src_dir/riru/out/safetynet-fix-"*.zip
 
-version="$(grep '^version=' module.prop  | cut -d= -f2)"
-
 rm -f "$src_dir/safetynet-fix-v"*.zip
 
-unzip "$src_dir/java_riru/app/build/outputs/apk/release/app-release.apk" classes.dex
-
-unzip "$src_dir/java_zygisk/app/build/outputs/apk/release/app-release.apk" -d "$tmp_dir/java_zygisk/classes.dex"
-mv "$tmp_dir/java_zygisk/classes.dex" zygisk_classes.dex
-
-wget -P "$tmp_dir/wsfn" https://github.com/kdrag0n/safetynet-fix/releases/download/v2.4.0/safetynet-fix-v2.4.0.zip
-unzip "$tmp_dir/wsfn/safetynet-fix-v2.4.0.zip" -d "$tmp_dir/sfn"
-mv "$tmp_dir/sfn/zygisk" ./
+version="$(grep '^version=' module.prop  | cut -d= -f2)"
 
 sha256sum classes.dex | cut -d' ' -f1 | tr -d '\n' > classes.dex.sha256sum
 sha256sum zygisk_classes.dex | cut -d' ' -f1 | tr -d '\n' > zygisk_classes.dex.sha256sum
