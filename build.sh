947892b52e2613da2127b423394b641ad58b74e0
Misael Filho <jukmisael@gmail.com>
2023-01-17 23:09:48 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -42,8 +42,8 @@ unzip "$src_dir/java_riru/app/build/outputs/apk/release/app-release.apk" classes
 unzip "$src_dir/java_zygisk/app/build/outputs/apk/release/app-release.apk" -d "$tmp_dir/java_zygisk/classes.dex"
 mv "$tmp_dir/java_zygisk/classes.dex" zygisk_classes.dex
 
-wget -P "$tmp_dir/sfn" https://github.com/kdrag0n/safetynet-fix/releases/download/v2.4.0/safetynet-fix-v2.4.0.zip
-unzip "$tmp_dir/safetynet-fix-v2.4.0.zip" -d "$tmp_dir/sfn"
+wget -P "$tmp_dir/wsfn" https://github.com/kdrag0n/safetynet-fix/releases/download/v2.4.0/safetynet-fix-v2.4.0.zip
+unzip "$tmp_dir/wsfn/safetynet-fix-v2.4.0.zip" -d "$tmp_dir/sfn"
 mv "$tmp_dir/sfn/zygisk" ./
 
 sha256sum classes.dex | cut -d' ' -f1 | tr -d '\n' > classes.dex.sha256sum
